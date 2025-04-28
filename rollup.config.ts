80fae4ac4418e7aa20c26758f87a893241866f6e
Rickard Andersson <dunklas@gmail.com>
2025-04-28 10:39:54 +0200

    Run prettier

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,18 @@
 // See: https://rollupjs.org/introduction/
 
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 
 const config = {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
     esModule: true,
-    file: "dist/index.js",
-    format: "es",
-    sourcemap: true,
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
-};
+  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
+}
 
-export default config;
+export default config
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { run } from "./main.js";
+import { run } from './main.js'
 
-run();
+run()
